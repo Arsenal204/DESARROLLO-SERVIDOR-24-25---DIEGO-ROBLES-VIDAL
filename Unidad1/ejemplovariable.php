5a027ee8e8bd99232b4258ebc7f0fce24423c837
Arsenal204 <diegorobles031204@gmail.com>
2024-09-25 13:32:08 +0200

    Update ejemplovariable.php

--- Unidad1/ejemplovariable.php
+++ Unidad1/ejemplovariable.php
@@ -8,10 +8,13 @@
 <body>
     <?php
         // Al declarar variables no se puede empezar con un número ni con algún caracter especial
-        $num1 = 23;
-        $num1 = "ventitres";
+        $num1 = 23  . "40";
+        $num2 = "23";
+        $num3=$num1+$num2;
 
-        $ats=34;
+        print "<br/>La suma de 23 y 23 es $num3";
+
+        $texto="<br/> estamos en segundo de daw";
 
         print $num1;
 
@@ -35,11 +38,23 @@
         //count nos cuenta la cantidad de elementos de un array
         print("<br/>El array empleado tiene " . count($empleado)." elementos");
 
-        var_dump($empleado)
+        var_dump($empleado);
+
+        //str_replace ("palabra a buscar", "palabra sustituta", Cadena de donde se va a cambiar)
+        echo $texto;
+        echo str_replace("daw","smr",$texto);
+
+        //strpos() Te da la posición de la palabra que tu le diga, aunque solo te da la posición de la primera vez que aparezca
 
         //strtok Te separa un texto según el elemento que le hayas dicho
 
         //strcom(); La mayor es segun el codigo ASCII
+
+        //trim para quitar espacios de delante y detras
+
+        //explode cçrta una cadena y devuelve un array de secciones de dicha cadena
+        echo "<br/>";
+        var_dump( explode(" ",$texto));
 
         //Las variables creadas dentro de funtion solo se pueden utilizar dentro de funtion
     ?>
