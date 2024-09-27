6f62c2a19e54f7e74b2de97a091756790689ea05
Arsenal204 <diegorobles031204@gmail.com>
2024-09-27 14:41:34 +0200

    Create pruebas.php

---
+++ Unidad1/pruebas.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=ç, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $a=200;
+        $b= 300;
+        echo $a."300" + $b ."</br>";
+        $gatos = array("gussy","gummy","melocotón");
+$cantidad=count($gatos)+2;
+echo $cantidad;
+$cantidad *=1111;
+echo $cantidad."</br>";
+       
+$a="20";
+$b="3";
+echo $a+$b."</br>";
+
+$string = "Me encanta DAW";
+$lon=strlen($string);
+echo ++$len."</br>";
+
+echo 'A';
+$b='B';
+echo 'B';
+echo 'C';
+
+
+    ?>
+</body>
+</html>
