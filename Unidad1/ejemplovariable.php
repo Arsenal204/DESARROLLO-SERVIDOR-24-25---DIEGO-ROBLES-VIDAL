bc13beb582ea5aa99ca084a504e32403ab530c7d
Arsenal204 <diegorobles031204@gmail.com>
2024-09-27 09:27:20 +0200

    Update ejemplovariable.php

--- Unidad1/ejemplovariable.php
+++ Unidad1/ejemplovariable.php
@@ -7,14 +7,30 @@
 </head>
 <body>
     <?php
+
+        define("VIDA_MAXIMA",100);
         // Al declarar variables no se puede empezar con un número ni con algún caracter especial
         $num1 = 23  . "40";
         $num2 = "23";
         $num3=$num1+$num2;
+        $array_numeros=array(12,45,92,27,9,2,7);
+        $num4=23;
+
 
         print "<br/>La suma de 23 y 23 es $num3";
 
         $texto="<br/> estamos en segundo de daw";
+
+        echo "</br>";
+
+        print PHP_INT_MAX . "</br>";
+
+        if (is_int($num2)."</br>"){
+            print"es entero";
+        }else{
+               print "no es entero";
+            }
+        ;
 
         print $num1;
 
@@ -57,6 +73,28 @@
         var_dump( explode(" ",$texto));
 
         //Las variables creadas dentro de funtion solo se pueden utilizar dentro de funtion
+
+
+        print "</br>".min($array_numeros);
+
+        print "</br>". round(4.5);
+
+        //No pilla los numeros decimales al utilizar la función rand
+
+        print "<br/> Numero aleatorio ".rand(-0.0,3.50); 
+
+        print"<br/> La vida maxima es: ". VIDA_MAXIMA;
+
+        if($num2==$num4){
+            print"</br> Son iguales";
+        };
+        if($num4===$num2){
+            print"</br> Son identicos";
+        }else print "</br> No son identicos";
+        
+        //print_r Saca los valores de un array (como el var_bump pero más bonito)
+
+        $valor=($num2+$num2)/2;
     ?>
 </body>
 </html>
