3049d48fbe4d2b2aa170e10e0195baa4a6af0b74
Arsenal204 <diegorobles031204@gmail.com>
2024-10-23 18:20:58 +0200

    23/10/2024

--- Unidad2/Ejercicios Unidad 2/Ejercicio3/ejercicio3.php
+++ Unidad2/Ejercicios Unidad 2/Ejercicio3/ejercicio3.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Juego de Adivinar el Número</title>
 </head>
+
 <body>
     <h1>¡Adivina el Número!</h1>
-    <p>Adivina un número entre 1 y 100. Tienes 6 intentos.</p>
-    
-    <?php 
+
+    <?php
 
     /**
      * Crea un programa que genere un número aleatorio y haga que el usuario lo intente acertar en menos de 6 intentos. 
@@ -17,41 +18,51 @@
      * si está más lejos dirá que el número es mayor o menor.
      *
      */
-    
-    $numaleatorio=rand(1,100);
-    $numdado=0;
-    echo '<form method="post"><label type="number" class="form-label">Elige un número entre 1 y 100</label> <input type="name" class="form-control input-small" id="numdado" name="numdado" aria-describedby="Numero dado"</form>';
-    echo '<form method="post"><button type="submit">¿He ganado?</button></form>';
-   
-    for ($i=0; $i < 6; $i++) { 
-        
-        if ($numaleatorio==$numdado){
-            echo "Has ganado";
-        }elseif (abs($numdado - $numaleatorio) < 5|| abs($numdado - $numaleatorio) > -5){
-            echo "Calentito totalis!";
-        }elseif ($numdado>$numaleatorio){
-            echo "El número es menor";
-        }else {
-            echo "El número es mayor";
-        }
-        echo '<form method="post"><label type="name" class="form-label">Elige un número entre 1 y 100</label> <input type="name" class="form-control input-small" id="numdado" name="numdado" aria-describedby="Numero dado"</form>';
-        echo '<form method="post"><button type="submit">¿He ganado?</button></form>';
+    if (!isset($_POST['numdado'])) {
+        $numaleatorio = rand(1, 100);
+        $numdado = 0;
+        $intento = 6;
+    } else {
+        $numdado = $_POST['numdado'];
+        $intento = $_POST['intento'];
+        $numaleatorio = $_POST['numaleatorio'];
     }
-        
-        
 
-
-
+    if ($intento != 0) {
+        if (isset($_POST['numdado'])) {
+            $numdado = $_POST['numdado'];
+            if ($numaleatorio == $numdado) {
+                echo "Has ganado";
+            } elseif (abs($numdado - $numaleatorio) <= 5  && abs($numdado - $numaleatorio) >= -5) {
+                echo "Calentito totalis!";
+            } elseif ($numdado > $numaleatorio) {
+                echo "El número es menor";
+            } else {
+                echo "El número es mayor";
+            }
+            $intento--;
+        }
+        "<p>Adivina un número entre 1 y 100. Tienes <?php echo $intento; ?> intentos.</p>";
+    }
+    var_dump($numaleatorio);
+    var_dump($numdado);
 
     ?>
-    
+    <form method="post"><label type="number" class="form-label">Elige un número entre 1 y 100</label>
+        <input type="hidden" value="<?php echo $numaleatorio ?>" type="number" class="form-control input-small" id="numaleatorio" name="numaleatorio" aria-describedby="Numero aleatorio">
+        <input value="<?php echo $numdado ?>" type="number" class="form-control input-small" id="numdado" name="numdado" aria-describedby="Numero dado">
+        <input type="hidden" value="<?php echo $intento ?>" type="number" class="form-control input-small" id="intento" name="intento" aria-describedby="Intentos">
+        <button type="submit">¿He ganado?</button>
+    </form>
+
+
 
 </body>
 
 
-      
-    
-    
-  
+
+
+
+
 
 </html>
