b5cc486d98ab638434e91feaedfbaf56899eb5d8
Arsenal204 <diegorobles031204@gmail.com>
2024-10-25 00:23:23 +0200

    25/10/2024

--- Unidad2/Ejercicios Unidad 2/Ejercicio5/ejercicio5.php
+++ Unidad2/Ejercicios Unidad 2/Ejercicio5/ejercicio5.php
@@ -6,6 +6,11 @@
     <title>Document</title>
 </head>
 <body>
+    <?php 
+     echo "<form action=\"\" method=\"post\"><input type=\"submit\" name=\"jugar\" value=\"Cambio de todas las cartas\"></form>";
+   
+    ?>
+
     
 
 <?php 
@@ -18,7 +23,72 @@
  * (solo dos colores y hasta 5 para que sean menos imágenes)
  */
 
+
+// Inicialización de colores y números
+$colores = ['rojo', 'azul'];
+$numeros = [1, 2, 3, 4, 5];
+
+// Si no hay carta volteada (primera carga)
+if (!isset($_POST['carta_volteada_color'])) {
+    // Generar la carta volteada de manera manual
+    $carta_volteada_color = $colores[rand(0, 1)];
+    $carta_volteada_numero = rand(1, 5);
+} else {
+    // Recuperar la carta volteada enviada por el formulario
+    $carta_volteada_color = $_POST['carta_volteada_color'];
+    $carta_volteada_numero = $_POST['carta_volteada_numero'];
+}
+
+// Generar la mano del jugador de manera manual
+$mano_jugador = [];
+for ($i = 0; $i < 4; $i++) {
+    $mano_jugador[$i]['color'] = $colores[rand(0, 1)];
+    $mano_jugador[$i]['numero'] = rand(1, 5);
+}
+
+// Verificar si se ha seleccionado una carta válida
+$mensaje = '';
+if (isset($_POST['carta_color']) && isset($_POST['carta_numero'])) {
+    $carta_color = $_POST['carta_color'];
+    $carta_numero = $_POST['carta_numero'];
+
+    // Validar si la carta seleccionada es del mismo color o número
+    if ($carta_color == $carta_volteada_color || $carta_numero == $carta_volteada_numero) {
+        $mensaje = "¡Felicitaciones! Has seleccionado una carta válida.";
+    } else {
+        $mensaje = "Carta inválida. Intenta de nuevo.";
+    }
+}
 ?>
+
+<h1>Juego del Uno Simplificado</h1>
+
+<h2>Carta Volteada</h2>
+<p>Carta: <?php echo "<img src=\"img/" . $carta_volteada_numero . "_" . $carta_volteada_color . ".png\" alt=\"No se ha encontrado la imagen\">" ?></p>
+
+<h2>Tu Mano</h2>
+<form method="post">
+    <?php foreach ($mano_jugador as $i => $carta): ?>
+        <button type="submit" name="carta_color" value="<?php echo "<button name=\"carta1\"><img src=\"img/" . $carta['numero'] . "_" . $carta['color'] . ".png\" alt=\"No se ha encontrado la imagen\"></button>"; ?>">
+        <input type="hidden" name="carta_color" value="<?php echo $carta['color']; ?>">    
+        <input type="hidden" name="carta_numero" value="<?php echo $carta['numero']; ?>">
+            <input type="hidden" name="carta_volteada_color" value="<?php echo $carta_volteada_color; ?>">
+            <input type="hidden" name="carta_volteada_numero" value="<?php echo $carta_volteada_numero; ?>">
+            
+        </button>
+        <br>
+    <?php endforeach; ?>
+</form>
+
+<h2><?php echo $mensaje; ?></h2>
+
+<form method="post">
+    <input type="hidden" name="carta_volteada_color" value="<?php echo $carta_volteada_color; ?>">
+    <input type="hidden" name="carta_volteada_numero" value="<?php echo $carta_volteada_numero; ?>">
+    
+    <button type="submit" name="reiniciar_mano">Reiniciar Mano</button>
+</form>
+
 
 
 </body>
